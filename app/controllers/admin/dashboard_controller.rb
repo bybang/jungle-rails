e50ade8e49fbb3ef30b9668c7a19099990a72560
bybang <bybang26@gmail.com>
2022-02-02 23:23:18 -0500

    Add some logic to count total products and categories in dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
                                password: ENV["HTTP_BASIC_PASSWORD"]
 
   def show
+    @products_total = Product.count.inspect
+    @categories_total = Category.count.inspect
   end
 end
